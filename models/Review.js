5cd6bf4046e225a74189e9a38b0b53a3f24f8c71
Zack <zachary.berkley@gmail.com>
2022-12-20 21:13:27 -0500

    Added User and Review models

---
+++ models/Review.js
@@ -0,0 +1,42 @@
+// taking the Model and DataTypes (so we know the data type is a string/integer) object sequelize gives out; destructuring
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connect");
+
+class Review extends Model {
+  // extra methods will go here
+}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    // when the table name is pluralized, it leads to some issues in sequelize, so the table name must be "frozen"
+    freezeTableName: true,
+    underscored: true,
+    modelName: "playlist",
+  }
+);
+
+module.exports = Playlist;
---
+++ models/User.js
@@ -0,0 +1,61 @@
+// taking the Model and DataTypes (so we know the data type is a string/integer) object sequelize gives out; destructuring
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  checkPassword(passwordAttempt) {
+    return bcrypt.compareSync(passwordAttempt, this.password);
+  }
+}
+
+// calls the init method that our model inherited from sequelize
+User.init(
+  // describes all the different properties of User
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 24],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (userData) => {
+        userData.password = await bcrypt.hash(userData.password, 7)
+        return userData
+      },
+      beforeUpdate: async (userData) => {
+        userData.password = await bcrypt.hash(userData.password, 7)
+        return userData
+      }
+    },
+    sequelize,
+    timestamps: true,
+    // when the table name is pluralized, it leads to some issues in sequelize
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
