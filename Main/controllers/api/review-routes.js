935be283309e7185e5feb1ae7d9037ae3aaea973
Tessa <tess.starkweather@gmail.com>
2023-01-02 20:28:23 -0500

    added authentication and session info to routes

--- Main/controllers/api/review-routes.js
+++ Main/controllers/api/review-routes.js
@@ -1,55 +1,84 @@
 const router = require('express').Router();
 const { Review } = require('../../models');
+const { withAuth } = require('../../utils/auth');
 
-// get all reviews
-router.get('/', (req, res) => {
-    console.log("gathering all the lil review posts for you brb");
-    Review.findAll({
-       attributes: ["id", "isbn", "title", "text", "user_id"],
-    })
-    .then((dbReviewData) => res.json(dbReviewData))
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
+// Post a review
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newReview = await Review.create(req.body)
+        res.json(newReview)
+    }
+    catch(err) {
+        res.json(err);
+    }
+})
 
-// post a review
-// do we want to force users to log in before posting a review?
-router.post('/', (req, res) => {
-    console.log("otw to post your review brb");
-    Review.create({
-        title: req.body.title,
-        text: req.body.text,
-        user_id: req.session.user_id
-    })
-    .then((dbReviewData) => res.json(dbReviewData))
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-});
-
-// delete a review
-router.delete('/:id', (req, res) => {
-    Review.destroy({
-        where: {
-            id: req.params.id,
-        },
-    })
-    .then((dbReviewData) => {
-        if (!dbReviewData) {
-            res.status(404).json({
-                message: "No post found with this id"
-            });
-            return;
+// Get Review by ID
+router.get('/:id', async (req, res) => {
+    try{
+        const result = await Review.findByPk(req.params.id);
+        if (result) {
+            const review = result.get({plain:true});
+            res.status(200).json(review);
+        } else {
+            res.status(404).json({"error": "Review not found"});
         }
-        res.json(dbReviewData);
-    })
-    .catch((err) => {
-        console.log(err);
+    }
+    catch(err) {
         res.status(500).json(err);
-    });
+    }
+})
+
+// Get all Reviews
+router.get('/', async (req, res) => {
+    try{
+        const result = await Review.findAll();
+        const reviews = result.map((review) => {
+            return review.get({plain: true})
+        })
+        res.status(200).json(reviews);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update review
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const result = await Review.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        if (!result) {
+            res.status(404).json({"message": "Review not found"})
+        }
+        res.status(200).json(result);
+
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+// Delete a review
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const result = await Review.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        if (!result) {
+            res.status(404).json({"message": "Review not found"})
+        }
+        res.status(200).json(result);
+
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
