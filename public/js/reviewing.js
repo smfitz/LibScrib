727fbec1dd5e6a6996c48c1002bb58f2ac1eb569
Tessa <tess.starkweather@gmail.com>
2023-01-09 18:58:31 -0500

    db and review page improvements

--- public/js/reviewing.js
+++ public/js/reviewing.js
@@ -2,6 +2,12 @@ console.log("linked");
 var pathArray = window.location.pathname.split( '/' );
 console.log(pathArray);
 var pathIsbn = pathArray[2];
+console.log(pathIsbn);
+
+var bookIMG = document.querySelector('.book-image');
+// var bookDesc = document.querySelector('.book-description');
+
+
 
 async function bookInfoFetch(event) {
     const response = await fetch('/api/books/' + pathIsbn, {
@@ -10,6 +16,18 @@ async function bookInfoFetch(event) {
     });
     const data = await response.json();
     console.log(data);
+
+    appendBook(data);
 }
 
 bookInfoFetch();
+
+var appendBook = function (data) {
+    console.log('appending book data');
+    console.log("DATA", data);
+
+    var imageLink = data.image;
+
+    bookIMG.setAttribute("src", imageLink);
+
+}
