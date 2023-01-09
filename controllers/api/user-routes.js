f8a28c7275f888e52c9107f65f4036790e957c51
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-01-08 19:01:23 -0500

    restructured content

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,32 +1,47 @@
 const router = require('express').Router();
 const { User, Review } = require('../../models');
 
-// User Login
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({
-        where: { email: email }
-    })
-    if (!user) {
-        res.status(404).json({ message: "User not found"})
-    } else {
-        if (user.checkPassword(password)) {
-            // CORRECT password
-            req.session.user_id = user.id
-            req.session.logged_in = true
-            res.json({message: "User logged in"})
-        } else {
-            // INCORRECT password
-            res.json({message:"Password is incorrect"})
-        }
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
     }
-})
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // Create a new user
 router.post('/', async (req, res) => {
     try{
-        const result = await User.create(req.body);
-        res.json(result);
+        const userData = await User.create(req.body);
+         req.session.save(() => {
+           req.session.user_id = userData.id;
+           req.session.logged_in = true;
+
+           res.json({ user: userData, message: "You are now logged in!" });
+         });
     }
     catch(err) {
         res.json(err)
---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
+    }
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
